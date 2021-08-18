2dd5659167297e291e131f216805ef8770105fb0
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-08-18 15:33:25 -0500

    commit2

--- Technology/SmartPhone.cs
+++ Technology/SmartPhone.cs
@@ -5,11 +5,24 @@ namespace Technology
     {
         //TODO: For a child class: add at least 1 additional field and 1 additional method.
         public string Carrier { get; set; }
+        public bool ServiceActivated { get; set; }
         public readonly string Brand = "Apple";
 
-        public SmartPhone(int storageInGB, int memoryInMB, string processor, string carrier) : base(storageInGB, memoryInMB, processor)
+        public SmartPhone(int storageInGB, int memoryInMB, string processor, string carrier, bool serviceActivated) : base(storageInGB, memoryInMB, processor)
         {
             Carrier = carrier;
+            ServiceActivated = serviceActivated;
         }
+
+        public void ActivateService()
+        {
+            ServiceActivated = true;
+        }
+
+        public void DeactivateService()
+        {
+            ServiceActivated = false;
+        }
+
     }
 }
