022419ae37377862380b05eb148699b4d968cc5e
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-08-18 15:40:26 -0500

    commit3

---
+++ TechnologyTests/TechnologyTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Technology;
+
+namespace TechnologyTests
+{
+    [TestClass]
+    public class TechnologyTests
+    {
+        //TODO: Try to add three MSTests tests per class.
+
+        [TestMethod]
+        public void EmptyTest()
+        {
+            Assert.AreEqual(10, 10, .001);
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
